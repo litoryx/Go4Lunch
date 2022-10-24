629e08459f603a6ea2ecbd0d722358bdc4456c7d
Mickael <mickael.jean.20@gmail.com>
2022-10-24 14:38:36 +0200

    "Ajout Maps"

--- app/src/main/java/com/example/go4lunch/GeoFragment.java
+++ app/src/main/java/com/example/go4lunch/GeoFragment.java
@@ -2,12 +2,16 @@ package com.example.go4lunch;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 
 /**
@@ -15,7 +19,7 @@ import com.google.android.gms.maps.SupportMapFragment;
  * Use the {@link GeoFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class GeoFragment extends Fragment {
+public class GeoFragment extends Fragment implements OnMapReadyCallback {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -41,10 +45,23 @@ public class GeoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_geo, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        assert mapFragment != null;
+        mapFragment.getMapAsync( this);
+    }
+
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
 
     }
 }
